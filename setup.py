f687a172e4f23507a68f2a615cff8321e9240802
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-08-22 11:02:41 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ for name, requirements in extras_require.items():
 
 setup(
     name='WTForms-Alchemy',
-    version='0.7.12',
+    version='0.7.13',
     url='https://github.com/kvesteri/wtforms-alchemy',
     license='BSD',
     author='Konsta Vesterinen',
