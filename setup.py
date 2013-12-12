a92c391792d12b075ba2165fc8042810286c3c69
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-12-12 10:11:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ for name, requirements in extras_require.items():
 
 setup(
     name='WTForms-Alchemy',
-    version='0.9.2',
+    version='0.9.3',
     url='https://github.com/kvesteri/wtforms-alchemy',
     license='BSD',
     author='Konsta Vesterinen',
--- wtforms_alchemy/__init__.py
+++ wtforms_alchemy/__init__.py
@@ -47,7 +47,7 @@ __all__ = (
 )
 
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 
 class ModelFormMeta(FormMeta):
