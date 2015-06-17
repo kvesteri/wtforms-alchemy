40198315f3bfac6ebe79b8d0fa2fafb08ed07e6a
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-06-17 19:56:53 +0300

    Pin natsort requirement

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ extras_require = {
         'flake8>=2.4.0',
         'flexmock>=0.9.7',
         'isort>=3.9.6',
+        'natsort==3.5.6',
         'WTForms-Test>=0.1.1'
     ],
     'babel': ['Babel>=1.3'],
